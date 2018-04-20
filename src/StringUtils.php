daaad925fabfa9d8986ef9f26a46da65ee3dcbd4
François-Xavier MAURICARD <fxmauricard@gmail.com>
2018-04-20 15:17:29 +0200

    Added 'unCamelCase' method + some PHPDoc aligments.

--- src/StringUtils.php
+++ src/StringUtils.php
@@ -10,10 +10,10 @@ class StringUtils
     /**
      * Method that checks if a string starts with another one.
      *
-     * @param  string  $haystack The string in which we're searching in.
-     * @param  string  $needle   The string we are searching.
-     * @param  boolean $case     If it's case sensitive or not.
-     * @return boolean If the haystack starts by the needle.
+     * @param   string  $haystack   The string in which we're searching in.
+     * @param   string  $needle     The string we are searching.
+     * @param   boolean $case       If it's case sensitive or not.
+     * @return  boolean             If the haystack starts by the needle.
      */
     public static function startsWith($haystack, $needle, $case = true)
     {
@@ -24,10 +24,10 @@ class StringUtils
     /**
      * Method that checks if a string ends with another one.
      *
-     * @param  string  $haystack The string in which we're searching in.
-     * @param  string  $needle   The string we are searching.
-     * @param  boolean $case     If it's case sensitive or not.
-     * @return boolean If the haystack ends by the needle.
+     * @param   string  $haystack   The string in which we're searching in.
+     * @param   string  $needle     The string we are searching.
+     * @param   boolean $case       If it's case sensitive or not.
+     * @return  boolean             If the haystack ends by the needle.
      */
     public static function endsWith($haystack, $needle, $case = true)
     {
@@ -38,9 +38,9 @@ class StringUtils
     /**
      * Method that returns a substring from the start of the haystack to the needle.
      *
-     * @param  string $haystack The string in which we're searching in.
-     * @param  string $needle   The string we are searching.
-     * @return string The string.
+     * @param   string  $haystack   The string in which we're searching in.
+     * @param   string  $needle     The string we are searching.
+     * @return  string              The string.
      */
     public static function cutBefore($haystack, $needle)
     {
@@ -51,9 +51,9 @@ class StringUtils
     /**
      * Method that returns a substring from the end of the haystack to the needle.
      *
-     * @param  string $haystack The string in which we're searching in.
-     * @param  string $needle   The string we are searching.
-     * @return string The string.
+     * @param   string  $haystack   The string in which we're searching in.
+     * @param   string  $needle     The string we are searching.
+     * @return  string              The string.
      */
     public static function cutAfter($haystack, $needle)
     {
@@ -63,11 +63,11 @@ class StringUtils
     }
 
     /**
-     * Returns the className of an object without its namespace
+     * Method that returns the class name of an object without its namespace.
      *
-     * @param $object
-     * @param bool $lowerCase
-     * @return string
+     * @param   mixed   $object     The object for which we want the class name.
+     * @param   bool    $lowerCase  If the class name should have the first char to lower case or not.
+     * @return  string              The class name.
      */
     public static function getClassName($object, $lowerCase = false)
     {
@@ -75,6 +75,22 @@ class StringUtils
         if ($lowerCase) {
             $className = lcfirst($className);
         }
+
         return $className;
+    }
+
+    /**
+     * Method that reverses a camel case string.
+     *
+     * @param   string  $content    The camel case string.
+     * @param   string  $glue       The glue for the compound words. Defaults to '_'.
+     * @return  string              The un-camelcased string.
+     */
+    public static function unCamelCase($content, $glue = '_')
+    {
+        $content = preg_replace('#(?<=[a-zA-Z])([A-Z])(?=[a-zA-Z])#e', "'$glue' . strtolower('$1')", $content);
+        $content{0} = strtolower($content{0});
+
+        return $content;
     }
 }
